a43acdb3d502ac7d3c126ca44f8721e313de14b6
meghashreebb <70872708+meghashreebb@users.noreply.github.com>
2021-05-22 22:03:11 +0530

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+	// your code goes here
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    sort(a,a+n);
+	    cout<<a[0]*(n-1)<<endl;
+	}
+	return 0;
+}
